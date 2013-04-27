d3fc8a8c301e441e268290313abdbf349dd5a423
Sasha Crofter <sasha@crofter.org>
2013-04-26 20:34:54 -0400

    Remove unnecessary function declarations in log.js

--- log.js
+++ log.js
@@ -5,21 +5,19 @@
 // appendLog appends to the log by prepending the timestamp and
 // sending the message to the console.
 function appendLog(message) {
-	// timestamp returns the current date and time in a string form,
-	// which is used for logging with the console.
-	function timestamp() {
-		var date = new Date();
-		// getCurrentDate returns the current date in a String form.
-		function getCurrentDate() {
-			return (String(date.getMonth())+"."+String(date.getDay())+"."+String(date.getFullYear()));
-		}
-		// getCurrentTime returns the current time in a String form.
-		function getCurrentTime() {
-			return (String(date.getHours())+":"+String(date.getMinutes())+":"+String(date.getSeconds()));
-		}
-		return (getCurrentDate()+" at "+getCurrentTime()+" ==> ");
-	}
-	console.log(timestamp()+message);
+	console.log(getCurrentDate()+" at "+getCurrentTime()+" ==> "+message);
+}
+
+// getCurrentDate returns the current date in a String form.
+function getCurrentDate() {
+	var date = new Date();
+	return (String(date.getMonth())+"."+String(date.getDay())+"."+String(date.getFullYear()));
+}
+
+// getCurrentTime returns the current time in a String form.
+function getCurrentTime() {
+	var date = new Date();
+	return (String(date.getHours())+":"+String(date.getMinutes())+":"+String(date.getSeconds()));
 }
 
 exports.appendLog = appendLog;
