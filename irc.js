d54bcbf99e8470a7683d115e26aea34298e2367e
Luke Evers <me@lukevers.com>
2013-05-12 17:58:44 -0400

    More modularity! It's coming along nicely.

--- irc.js
+++ irc.js
@@ -6,6 +6,7 @@ var util = require("util");
 var irc = require("irc");
 var request = require("request");
 var c = require("./config");
+var fs = require('fs');
 
 util.log("Configured "+c.config.botName);
 util.log("Creating "+c.config.botName);
@@ -17,7 +18,18 @@ var bot = new irc.Client(c.config.server, c.config.botName, {
 util.log("Created "+c.config.botName);
 util.log("Connecting to "+c.config.server);
 
-require('./packages/op.js')(bot);
+fs.readdir('./packages', function(err, files) {
+	for (var i = 0; i < files.length; i++) {
+		
+		//
+		// TODO: create a JSON list of packages people want and then
+		//       check it against each one
+		//
+		
+		util.log('Loading package: '+files[i].substring(0, files[i].length-3));
+		require('./packages/'+files[i])(bot);
+	}
+});
 
 
 // --------------------------------------------------------------------------- //
