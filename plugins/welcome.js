6e575f5737214ef7740edf1f8e82f6f8cf1bc5de
Luke Evers <me@lukevers.com>
2013-05-22 20:52:33 -0400

    Working on welcome.js plugin.
    Right now you can pretend to set the welcome message, but
    it doesn't set anything yet. I was just testing out getting
    the welcome message correct. Right now it just relays the
    welcome message back to you.

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -7,3 +7,29 @@
 //	
 //	Written by Luke Evers.
 //
+
+var util = require('util');
+var fs = require('fs');
+
+var commands = ['+setWelcomeMessage'];
+
+module.exports = function(bot) {
+	var users = require('../users.json');
+	
+	bot.addListener('message', function(from, to, text, message) {
+		if (typeof users[from] == 'undefined') {
+			isOP = false;
+		} else isOP = (users[from].mode == '+o');
+		if (message.args[1].indexOf('+setWelcomeMessage') == 0) {
+			if (isOP) setWelcomeMessage(message.args[0], message.args[1].substring(19));
+			else bot.say(message.args[0], from+': you do not have permission to do that!');
+		}
+	});
+	
+	// will do in a bit
+	function setWelcomeMessage(channel, message) {
+		bot.say(channel, message);
+	}
+	
+	return commands;
+}
