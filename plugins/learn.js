c1d2cccd18ed88e806a652cd2e02293c45539bc1
Luke Evers <me@lukevers.com>
2013-09-23 18:31:31 -0400

    Learn things with !learn
    IT does not yet unlearn things or listen for them yet.

--- plugins/learn.js
+++ plugins/learn.js
@@ -16,8 +16,65 @@ module.exports = function(bot) {
     
     bot.addListener('message', function(from, to, text, message) {
 	var channel = message.args[0];
-	
-	
+	var msg = message.args[1];
+		
+	if (msg.indexOf('!learn') == 0) {
+	    var overwrite = (msg.indexOf('overwrite') > -1);
+	    var global = (msg.indexOf('global') > -1);
+	    msg = msg.replace(/\\"/g, "'");
+	    var learn = msg.split('"');
+
+	    // Delete un-used stuff
+	    delete learn[0];
+	    for (var i = 1; i < learn.length; i++) {
+		if (learn[i].indexOf('overwrite') > -1 || 
+		    learn[i].indexOf('global') > -1 ||
+		    learn[i] == " as" || learn[i] == " as " ||
+		    learn[i] == "as")
+		    delete learn[i]
+	    }
+
+	    var remember = learn[1];
+	    var as;
+	    for (var i = 2; i < learn.length; i++) {
+		if (learn[i] == " " || learn[i] == ""
+		   || typeof learn[i] == 'undefined') continue;
+		else as = learn[i];
+	    }
+
+	    if (typeof remember == 'undefined' ||
+		typeof as == 'undefined') {
+		bot.say(channel, from+': !learn example: !learn "kittens are cool" as "cool" [global] [overwrite]');
+	    } else {
+		if (global){
+		    if (typeof book['global'] == 'undefined') {
+			book['global'] = {};
+		    }
+		    if (typeof book['global'][as] == 'undefined' || overwrite) {
+		 	book['global'][as] = remember;
+			bot.say(channel, 'Okay, I will (globally) remember '+remember+' as '+as);
+			writeFile(book);
+		    } else {
+			bot.say(channel, 'I already know '+as+' as something else! Use overwrite if you want to overwrite it.');
+		    }
+		} else {
+		    if (typeof book[channel] == 'undefined') {
+			book[channel] = {};
+		    }
+		    if (typeof book[channel][as] == 'undefined' || overwrite) {
+			bot.say(channel, 'Okay, I will remember '+remember+' as '+as);
+			book[channel][as] = remember;
+			writeFile(book);
+		    } else {
+			bot.say(channel, 'I already know '+as+' as something else! Use overwrite if you want to overwrite it.');
+		    }
+		}
+	    }
+	} else if (msg.indexOf('!unlearn') == 0) {
+	    
+	    // TODO
+	    
+	}
     });
     
     
