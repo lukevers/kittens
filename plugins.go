5d21f61d51badb281f851e91daa914e0570ed82e
Luke Evers <me@lukevers.com>
2013-09-26 08:23:45 -0400

    Comments for plugins.go

--- plugins.go
+++ plugins.go
@@ -4,8 +4,12 @@ import (
 	"github.com/fluffle/goirc/client"
 )
 
-// AddPlugins is a func that 
+// AddPlugins is a func that scans the plugin folder to see what
+// plugins are available. It then adds each plugin to the bot to use
+// once it connects to the server.
 func AddPlugins(bot *client.Conn) *client.Conn {
-
+	
+	// TODO
+	
 	return bot
 }
