b670238d768818135ba471f48d785dfbfeb432ef
Luke Evers <me@lukevers.com>
2014-06-27 09:34:05 -0400

    Added blocks for each field that can be changed

--- forms.go
+++ forms.go
@@ -37,6 +37,36 @@ func UpdateServer(w http.ResponseWriter, req *http.Request) {
 		server.Conn.Nick(server.Nick)
 	}
 
+	// Check if Real Name has been changed, and update it if it has
+	if req.Form["realname"][0] != server.RealName {
+		verbf("Changing real name to %s", req.Form["realname"][0])
+	}
+
+	// Check if Host has been changed, and update it if it has
+	if req.Form["host"][0] != server.Host {
+		verbf("Changing host to %s", req.Form["host"][0])
+	}
+
+	// Check if Server Name has been changed, and update it if it has
+	if req.Form["servername"][0] != server.ServerName {
+		verbf("Changing server name to %s", req.Form["servername"][0])
+	}
+
+	// Check if Network has been changed, and update it if it has
+	if req.Form["network"][0] != server.Network {
+		verbf("Changing network to %s", req.Form["network"][0])
+	}
+
+	// Check if Port has been changed, and update it if it has
+	if req.Form["port"][0] != server.Port {
+		verbf("Changing port to %s", req.Form["port"][0])
+	}
+
+	// Check if Password has been changed, and update it if it has
+	if req.Form["password"][0] != server.Password {
+		verbf("Changing password to %s", req.Form["password"][0])
+	}
+
 	// Redirect back to "/server/{id}" when we're done here
 	http.Redirect(w, req, "/server/"+strconv.Itoa(int(server.ID)), 303)
 }
