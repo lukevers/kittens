b42356f8752a2ff99ffa129eaf351fecfce75cf4
Luke Evers <me@lukevers.com>
2014-09-15 09:12:07 -0400

    Use our "checkbox" fontawesome icons everywhere
    Also center table cells
    Ref #53

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -10,10 +10,19 @@ $(function() {
 					RedirectIfFragmented();
 					break;
 				default:
+					break;
+			}
+			switch (location.href.split('/')[4])
+			{
+				case 'new':
+					FakeCheckboxs();
+					break;
+				default:
 					JoinChannel();
 					PartChannel();
 					EnableDisable();
 					ViewChannelOptions();
+					FakeCheckboxs();
 					break;
 			}
 			break;
