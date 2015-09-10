197a33ee38f686b5117b6dbb0837811241f4c239
Luke Evers <me@lukevers.com>
2015-09-10 08:16:37 -0400

    Adding csrf middleware back

--- router.go
+++ router.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	//"github.com/tommy351/gin-csrf"
+	"github.com/tommy351/gin-csrf"
 	"github.com/tommy351/gin-sessions"
 	"html/template"
 	"log"
@@ -22,10 +22,10 @@ func InitRouter() {
 	store = sessions.NewCookieStore([]byte(os.Getenv("USERS_COOKIE_STORE_SECRET")))
 	router.Use(gin.Logger())
 	router.Use(sessions.Middleware("session", store))
-	//router.Use(csrf.Middleware(csrf.Options{
-	//	Secret:    os.Getenv("USERS_CSRF_TOKEN_SECRET"),
-	//	ErrorFunc: CsrfMismatch,
-	//}))
+	router.Use(csrf.Middleware(csrf.Options{
+		Secret:    os.Getenv("USERS_CSRF_TOKEN_SECRET"),
+		ErrorFunc: CsrfMismatch,
+	}))
 
 	if os.Getenv("APP_DEBUG") != "true" {
 		gin.SetMode(gin.ReleaseMode)
