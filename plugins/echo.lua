58672dd7f57b175453113a39d52d4366f41494d6
Luke Evers <me@lukevers.com>
2015-10-21 19:10:47 -0400

    Echo plugin should match a space after echo.

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -1,7 +1,7 @@
 on("PRIVMSG", "echo")
 
 function echo(channel, message)
-    if string.find(message, "^@echo") then
-        say(channel, string.gsub(message, "^@echo", ""))
+    if string.find(message, "^@echo%s") then
+        say(channel, string.gsub(message, "^@echo%s", ""))
     end
 end
